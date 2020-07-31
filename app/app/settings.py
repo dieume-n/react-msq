01f9ac6da9669c6880e9c1330c4d56309b70a45e
Luteranya Nyange <dieume.dev@gmail.com>
2020-07-31 13:37:19 +0300

    user authentication using JWT

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     "tracks",
 ]
 
-GRAPHENE = {"SCHEMA": "app.schema.schema"}
+GRAPHENE = {
+    "SCHEMA": "app.schema.schema",
+    "MIDDLEWARE": ["graphql_jwt.middleware.JSONWebTokenMiddleware",],
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -51,6 +54,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+AUTHENTICATION_BACKENDS = [
+    "graphql_jwt.backends.JSONWebTokenBackend",
+    "django.contrib.auth.backends.ModelBackend",
 ]
 
 ROOT_URLCONF = "app.urls"
